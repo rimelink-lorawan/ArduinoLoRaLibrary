5814dba5b933d5aaa12701ace9c9e4c1a67739f8
rimelink-lorawan <55487528+rimelink-lorawan@users.noreply.github.com>
2021-07-08 14:50:29 +0800

    Update lora.cpp

--- lora.cpp
+++ lora.cpp
@@ -1,220 +1,295 @@
-//
-//    FILE: lora.cpp
-// VERSION: 0.1.00
-// PURPOSE: RimeLink LO0RA library for Arduino
-//
-// DATASHEET: 
-//
-// HISTORY:
-// 0.1.0 by Peter (15/12/2019)
-// inspired by LORA library
-//
+/**
+ * \file
+ *         lora.cpp
+ * \description
+ *         LoRa library for Arduino by RimeLink (www.rimelink.com)
+ * \date
+ *         2019-10-27 09:44 by Peter
+ *         2021-07-06 13:33 by John
+ * \copyright
+ *         Revised BSD License, see section \ref LICENSE
+ */
 
+/* Includes ------------------------------------------------------------------*/
 #include "lora.h"
+#include <stdlib.h>
+#include <SoftwareSerial.h>
 
-/////////////////////////////////////////////////////
-//
-// PUBLIC
-//
+extern SoftwareSerial    debugSerial;
+
+/* Compile switch-------------------------------------------------------------*/
+#define PRINT_STATUS_TIME    0  // 0=disable, 1=enable
+
+
 /* Private macro -------------------------------------------------------------*/
-#define RX_BUFFER_LEN   255
-#define MAX_LEN_UART_FRAME_DATA    (222+2)  // 2=MType+FPort, SF7~9=222
-#define COMM_TRM_HEAD    0x3Cu
-#define COMM_TRM_TAIL    0x0Du
-#define PTR_NULL    ((void *)0)
+#define MAX_SIZE_UART_PAYLOAD    (222 + 2)  // 2=MType+FPort, SF7~9=222
+#define MAX_SIZE_UART_BUF   (6 + MAX_SIZE_UART_PAYLOAD)  // 6=0xFF+Head+Type+Length+CS+Tail
 
-/**
-* @brief  Make response type of UART frame. 
-*/
-#define MAKE_UART_TYPE_RESP(byType)    (0x80u + (byType))
 
-/**
-* @brief  Type of received communication frame
-*/
-typedef enum
+/* Private variables -----------------------------------------------------------*/
+static uint8_t    rxtxBuf[MAX_SIZE_UART_BUF];
+static uint8_t    rxSizeInBuf = 0;
+	
+
+/*---------------------------------------------------------------------------*/
+int16_t lora::available(void)
 {
-    TYPE_INVALID_MIN = (byte)0,
-    TYPE_GET_VER=1, /*!< User System get version of this node. */
-    TYPE_TX_RF_DATA=2, /*!< User System send data that need to TX by RF. */
-    TYPE_GET_JOINEDSTATE=3, /*!< User System get Network Joined State. */
-    TYPE_TX_RF_DATA_BYTYPE=4, 
-    TYPE_CLASS=5,
-    TYPE_APPEUI=6,
-    TYPE_APPKEY=7,
-    TYPE_ADR=8,
-    TYPE_TX_PWR=9, 
-    TYPE_DATARATE=0x0a,
-    TYPE_CHANNEL=0x0b,
-    TYPE_INVALID_MAX=0x0C,
-    TYPE_WAKE_DATA = ((byte)0xC0), /*!< Node send wake data to User System. */
-} COMM_FRAME_TYPE_TypeDef;
-/**
-* @brief  Data object for received communication frame
-*/
-typedef struct
-{
-    byte    dataIdx;    /* Index of data field */
-    byte    dataLen;    /* Length of data field */
-    byte    frameLen; /* Length of frame */
-    bool    rx_ready;  /* Indicate whether the whole frame received */
-    byte    RxBuf[RX_BUFFER_LEN];
-} COMM_RX_DATA;
-
-typedef struct
-{
-    byte head;
-    byte cmd;
-    byte dl;    /* data length of rime lora->serial frame */
-    byte data;
-    byte tail;
-}FRAME_IDX;
-
-COMM_RX_DATA RxBuff={0, 0, 0, false, {0}};
-byte    s_abyTxUARTFrame[255];
-
-/* start-RX method of LoRa library */
-void lora::handleRxData(uint8_t inChar)
-{
-    COMM_RX_DATA *pRx=&RxBuff;
-    
-    if (pRx->frameLen>= RX_BUFFER_LEN) return;
-    if (!pRx->rx_ready) {
-       pRx->RxBuf[pRx->frameLen] = inChar;
-       pRx->frameLen++;
-    }
-}
-
-bool lora::rxDataReady(void)
-{
-    FRAME_IDX idx;
-    COMM_RX_DATA *pRx=&RxBuff;
-
-    for(byte i=0; i<pRx->frameLen; i++)
+    if (Serial.available())
     {
-        idx.head=i;
-        if(COMM_TRM_HEAD==pRx->RxBuf[idx.head]) /* Check Frame Head */
-        {
-            idx.cmd=idx.head+1;
-            if(TYPE_WAKE_DATA==pRx->RxBuf[idx.cmd]) /* Check Frame Command */
-            {
-                idx.dl=idx.head+2;
-                idx.data=idx.head+3;
-                idx.tail=idx.head+4+(pRx->RxBuf[idx.dl]); /* Calculate the index of Tail in rx Frame*/
-                if(COMM_TRM_TAIL==pRx->RxBuf[idx.tail]) /* Check Frame Tail */
-                {
-                    pRx->dataIdx = idx.data;
-                    pRx->dataLen = pRx->RxBuf[idx.dl];
-                    pRx->rx_ready = true;
-                    break;
-                } /* end of check frame tail */
-            } /* end of check frame command */
-        } /* end of check frame head */
-    } /* end of loop */
-    return (pRx->rx_ready);
-}
-
-bool lora::available(void)
-{
-    bool rtn=false;
-    while(Serial.available()) {
-        //get the new byte
-        char inChar = (char)Serial.read();
-        handleRxData(inChar);
-    }
-    if(rxDataReady()) {
-        rtn=true;
+        delay(10);  // 10ms=57Bytes@57600-8-n-1
     }
     else
     {
-        /* Clear RxBuff */
-        RxBuff.dataIdx=0;
-        RxBuff.dataLen=0;
-        RxBuff.frameLen=0;
-        RxBuff.rx_ready=false;
-        memset(RxBuff.RxBuf, 0, RX_BUFFER_LEN);
+        return 0;  // have NO data
     }
-    return rtn;
-}
 
-byte lora::read(void *p_bData, byte maxlen)
-{
-    byte copylen;
-    COMM_RX_DATA *pRx=&RxBuff;
-    
-    if ((PTR_NULL == p_bData) || (0 == maxlen) || (3 >= pRx->dataLen)) /* data field have 3 bytes RSSI+SNR at tail*/
-        return (-1);
-
-    /* Calculate memcpy length */
-    copylen = pRx->dataLen-3;
-    copylen = (copylen>maxlen)?maxlen:copylen;
-
-    memcpy(p_bData, &pRx->RxBuf[pRx->dataIdx], copylen);
-    pRx->dataIdx=0;
-    pRx->dataLen=0;
-    pRx->frameLen=0;
-    pRx->rx_ready=false;
-    memset(pRx->RxBuf, 0, RX_BUFFER_LEN);
-    return copylen;
-}
-/* end-RX method of LoRa library */
-
-/* start-TX method of LoRa library */
-int lora::write(const void *p_vData, int bySize)
-{
-    int byTxsize;
-
-    byTxsize = bySize + 2;
-    if (MAX_LEN_UART_FRAME_DATA < byTxsize)
+    /* get the RX frame from LoRa module */
+    rxSizeInBuf = 0;
+    while( Serial.available())
     {
-        return -1; /* Bad size of data. */
+        int    rxData = Serial.read();  // drain all data on Serial buffer
+        if (rxSizeInBuf < sizeof(rxtxBuf))
+        {
+            rxtxBuf[rxSizeInBuf++] = rxData;
+        }
     }
+
+#if 0 // only for debug
+    debugSerial.print("rxSizeInBuf=");
+    debugSerial.println(rxSizeInBuf);
+//    debugSerial.write(rxtxBuf, rxSizeInBuf);
+#endif
+
+    return rxSizeInBuf;
+}
+
+/*---------------------------------------------------------------------------*/
+int16_t lora::read(void *saveBuf, int16_t maxSize, int16_t *rxRSSI, int8_t *rxSNR)
+{
+    uint8_t    *startIndex, *endIndex, byte1, byte2;
+    int16_t    payloadSize, dataSize, copySize;
+
+    /* check all arguments is valid */
+    if (!saveBuf)
+    {
+        return -1;  // arguments error
+    }
+
+    /* find the start of downlink frame */
+    endIndex = &rxtxBuf[rxSizeInBuf] - 5; // 5=Head+Type+Length+CS+Tail
+    for (startIndex = &rxtxBuf[0]; startIndex < endIndex; ++startIndex)
+    {
+        if ((0x3C == startIndex[0]) && (0xC0 == startIndex[1])) // Head=0x3C, DownLink=0xC0
+        {
+            break;  // have found
+        }
+    }
+
+    if (endIndex <= startIndex)
+    {
+        return -2;  // Head or Type error
+    }
+
+    payloadSize = (int16_t)startIndex[2];  // [2]=Length
+    dataSize = payloadSize - 3; // RSSI=2Bytes, SNR=1Byte
+    if (dataSize < 0)
+    {
+        return -3;  // Size error
+    }
+
+    if (rxRSSI)
+    {
+        byte1 = startIndex[3 + dataSize + 0]; // 3=Head+Type+Length
+        byte2 = startIndex[3 + dataSize + 1];
+        *rxRSSI = (int16_t)((byte1 << 8) | byte2);
+    }
+	
+    if (rxSNR)
+    {
+        *rxSNR = (int8_t)startIndex[3 + dataSize + 2]; // 3=Head+Type+Length, 2=RSSI
+    }
+
+    copySize = (maxSize < dataSize) ? maxSize : dataSize;  // avoid overflow
+    if (0 < copySize)
+    {
+        memcpy(saveBuf, &startIndex[3], copySize); // 3=Head+Type+Length
+    }
+
+    return copySize;
+}
+
+/*---------------------------------------------------------------------------*/
+int16_t lora::read(void *saveBuf, int16_t maxSize)
+{
+    return read(saveBuf, maxSize, NULL, NULL);
+}
+
+/*---------------------------------------------------------------------------*/
+int16_t lora::write(const void *srcBuf, int16_t srcSize, bool isConfirmed, uint8_t byFPort)
+{
+    int16_t    txSize;
+
+    txSize = srcSize + 2; // 2=MType+FPort
+
+    /* check all arguments is valid */
+    if (!(srcBuf && (txSize <= MAX_SIZE_UART_PAYLOAD) && (1 <= byFPort && byFPort <= 199)))
+    {
+        return -1;  // arguments error
+    }
+
     /* Make head of frame. */
-    s_abyTxUARTFrame[0] = 0xff;
-    s_abyTxUARTFrame[1] = 0x3c;
-    s_abyTxUARTFrame[2] = 0x04;
-    s_abyTxUARTFrame[3] = byTxsize;
-    s_abyTxUARTFrame[4] = 0x00;
-    s_abyTxUARTFrame[5] = 0x64;
+    rxtxBuf[0] = 0xFF; // wake LoRa node
+    rxtxBuf[1] = 0x3C; // Head
+    rxtxBuf[2] = 0x04; // 4=Send LoRa Frame
+    rxtxBuf[3] = txSize;
+    rxtxBuf[4] = isConfirmed ? 1 : 0;
+    rxtxBuf[5] = byFPort;
 
     /* Copy payload into body of frame. */
-    if (p_vData && (0 < bySize))
+    if (srcBuf && (0 < srcSize))
     {
-        memcpy(&s_abyTxUARTFrame[6], p_vData, bySize);
+        memcpy(&rxtxBuf[6], srcBuf, srcSize);
     }
 
     /* Make tail of frame. */
-    s_abyTxUARTFrame[4 + byTxsize] = CalcCS(&s_abyTxUARTFrame[1], byTxsize + 3);
-    s_abyTxUARTFrame[5 + byTxsize] = 0x0d;
-    Serial.write(s_abyTxUARTFrame, byTxsize+6);
+    rxtxBuf[4 + txSize] = calc_cs(&rxtxBuf[1], txSize + 3); // 3=Head, Type, Size
+    rxtxBuf[5 + txSize] = 0x0D; // Tail
+    
+    Serial.write(rxtxBuf, txSize + 6); // 6=0xFF+Head+Type+Length+CS+Tail
     
     return 0;
 }
- 
 
-/////////////////////////////////////////////////////
-//
-// PRIVATE
-//
-
-// return values:
-//  0 : OK
-// -2 : timeout
-byte lora::CalcCS(const void *p_vBuf, int nSize)
+/*---------------------------------------------------------------------------*/
+int16_t lora::write(const void *srcBuf, int16_t srcSize)
 {
-    byte    bySum;
-    const byte    *p_byBuf;
+    return write(srcBuf, srcSize, false, 100);  // Unconfirmed, FPort=100
+}
+
+
+/*---------------------------------------------------------------------------*/
+int16_t lora::write(const void *srcBuf, int16_t srcSize, bool isConfirmed, uint8_t byFPort, char **saveStatus)
+{
+    #define MAXINSZ    24
+    static uint8_t    instr[MAXINSZ + 1];  // add 1 for '\0'
+
+    int16_t    index, result;
+    uint32_t    msStart;
+
+    result = write(srcBuf, srcSize, isConfirmed, byFPort);
+    if (result < 0)
+    {
+        return result;
+    }
+
+    /* waiting until reveived or timeout */
+    msStart = millis();
+    do
+    {
+        if (Serial.available())
+        {
+            break;
+        }
+    } while ((millis() - msStart) < 3000);
+
+#if PRINT_STATUS_TIME
+    uint32_t msWait = millis() - msStart;
+#endif
+
+    delay(10);  // 10ms=57Bytes@57600-8-n-1
+
+    /* get the responsed string from LoRa module */
+    index = 0;
+    while( Serial.available())
+    {
+        int    rxData = Serial.read();  // drain all data on Serial buffer
+        if (index < MAXINSZ)
+        {
+            instr[index++] = rxData;
+        }
+    }
+    instr[index - 2] = '\0';  // 2=CS+Tail
+
+    if (saveStatus)
+    {
+        *saveStatus = &instr[3];  // 3=Head+Type+Length
+    }
+
+#if PRINT_STATUS_TIME
+    debugSerial.print("WaitStatus:");
+    debugSerial.print(msWait);
+    debugSerial.println("ms");
+#endif
+
+    if (('T' == instr[3]) && ('X' == instr[4]) && (' ' == instr[5]) && ('O' == instr[6]) && ('K' == instr[7]))
+    {
+        return 0;
+    }
+    else
+    {
+        return -2;
+    }
+}
+
+/*---------------------------------------------------------------------------*/
+int16_t lora::write(const void *srcBuf, int16_t srcSize, bool isConfirmed, uint8_t byFPort, char **saveStatus, uint16_t msTimeOnAir)
+{
+    int16_t    result;
+    uint16_t    msTimeout;
+    uint32_t    msStart;
+
+    result = write(srcBuf, srcSize, isConfirmed, byFPort, saveStatus);
+    if (result < 0)
+    {
+        return result;
+    }
+
+    if (3000 < msTimeOnAir)
+    {
+        msTimeout = 3000;  // MAX time on Air is 3000ms
+    }
+    else
+    {
+        msTimeout = msTimeOnAir;
+    }
+    msTimeout += 2000;  // RX1=1000ms, RX2=2000ms
+
+    /* waiting until reveived or timeout */
+    msStart = millis();
+    do
+    {
+        if (Serial.available())
+        {
+            break;
+        }
+    } while ((millis() - msStart) < msTimeout);
+
+    return (millis() - msStart);
+}
+
+/*---------------------------------------------------------------------------*/
+uint8_t lora::calc_cs(const void *srcBuf, int16_t srcSize)
+{
+    uint8_t    bySum;
+    const uint8_t    *p_byBuf;
 
     bySum = 0;
-    p_byBuf = (const byte *)p_vBuf;
+    p_byBuf = (const uint8_t *)srcBuf;
     
-    while (nSize-- > 0)
+    while (0 < srcSize--)
     {
         bySum += *p_byBuf++;
     }
 
     return bySum;
 }
-/* end-TX method of LoRa library */
-//
-// END OF FILE
-//
+
+/*--------------------------------------------------------------------------
+                                                                       0ooo
+                                                           ooo0     (   )
+                                                            (   )      ) /
+                                                             \ (      (_/
+                                                              \_)
+----------------------------------------------------------------------------*/
+
+
